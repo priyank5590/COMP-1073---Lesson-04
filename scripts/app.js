7ba6b35b04d247c76937c409f816d16afdae0867
priyank5590 <vyaspriyank5@gmail.com>
2016-02-04 08:47:26 -0800

    javascripts files added

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,22 @@
 
 // setuo your IIFE (Immediately Invoked Function Expression)
 
-(function(){
-	
+(function () {
+
 	console.log("App Started...");
 	
-	var firstParagraph;
+	//declared a named function way
+	function replaceFirstParagraph() {
+		console.log("inside replaceFirstParagraph function");
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("firstParagraph");
+		firstParagraph.innerHTML = "My New Paragraph Data";
+	}
 	
-	firstParagraph = document.getElementById("firstParagraph");
-	firstParagraph.innerHTML = "My New Paragraph Data";
-	
-	
+	//call replace first paragraph function
+	replaceFirstParagraph();
+
+
+
 })();
